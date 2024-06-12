43ab6b97d5bc9f6c3343cf71da4a76b825cbff45
rizinho <135022344+rizinho@users.noreply.github.com>
2024-06-12 13:50:58 -0300

    bteste5

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,7 +1,43 @@
-export default function Inicio() {
-    return(
-      <div>
-        <h1>Iniciooooo</h1>
-      </div>
-    );
-  }
+'use client' 
+
+import {useState} from 'react'
+
+import styles from "./page.module.css"
+import Image from "next/image";
+
+export default function Home() {
+
+    const[email, setEmail] = useState('');
+    const[senha, setSenha] = useState('');
+
+    return (
+        <div className={styles.main}>
+
+            <header>
+                <div className={styles.header}>
+                    <div className={styles.logo}>
+
+                        <Image className={styles.icon}
+                            src="/logotcc.png"
+                            width={500}
+                            height={500}
+                            alt="Imagem do logo"
+                        />
+                        <h1 className={styles.txtLog}>Consult Doctor</h1>
+                    </div>
+                </div>
+            </header>
+
+            <div className={styles.container}>
+
+            </div>
+
+
+
+            <footer className={styles.roda}>
+                &copy; 2024 ConsultDoctor.com - Todos os direitos reservados
+            </footer>
+
+        </div>
+    )
+}
