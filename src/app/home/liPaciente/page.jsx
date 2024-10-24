a138c15164a7617979ac250860db1505cd142c0f
rizinho <135022344+rizinho@users.noreply.github.com>
2024-10-24 07:37:23 -0300

    bteste30

--- src/app/home/liPaciente/page.jsx
+++ src/app/home/liPaciente/page.jsx
@@ -1,7 +1,15 @@
+'use client'
+
+import styles from './page.module.css';
+import Header from '../../padrao/header/page';
+import Footer from '../../padrao/footer/page';
+
 export default function Home() {
     return(
-      <div>
+      <div className={styles.main}>
+        <Header/>
         <h1>PACI</h1>
+        <Footer/>
       </div>
     );
   }
