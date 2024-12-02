4e1c50caec17d5b5231f030b37557efb9cf4b0a4
lorenadasilva <135022342+lorenadasilva@users.noreply.github.com>
2024-12-02 14:41:36 -0300

    bteste44

--- src/app/home/telMedico/[id]/page.tsx
+++ src/app/home/telMedico/[id]/page.tsx
@@ -21,49 +21,44 @@ export default function DoctorProfile({ params: { id } }) {
       <Header />
       <div className={styles.container}>
         <div className={styles.backColor}>
-        <nav className={styles.breadcrumb}>
-          <Link href="/home">Início</Link> &gt; <span>{specialty}</span> &gt; <span>{name}</span>
-        </nav>
+          <nav className={styles.breadcrumb}>
+            <Link href="/home">Início</Link> &gt; <span>{specialty}</span> &gt; <span>{name}</span>
+          </nav>
 
-        <div className={styles.profile}>
-          {/* Card Esquerdo - Informações Básicas */}
-          <div className={styles.profileCard}>
-            <Image
-              src="/fotoPerf.jpg"
-              width={200}
-              height={200}
-              alt={`Foto de ${name}`}
-              className={styles.doctorImage}
-            />
+          <div className={styles.profile}>
+            {/* Card Esquerdo - Informações Básicas */}
+            <div className={styles.profileCard}>
+              <Image
+                src="/fotoPerf.jpg"
+                width={200}
+                height={200}
+                alt={`Foto de ${name}`}
+                className={styles.doctorImage}
+              />
+              <h1 className={styles.doctorName}>{name}</h1>
+              <p className={styles.doctorCrm}>CRM: {crm}</p>
+              <p className={styles.doctorSpecialty}>{specialty}</p>
+              <p className={styles.doctorLocal}>{location}</p>
+            </div>
 
-            <h2 className={styles.txtSob}>Sobre Mim</h2>
-            <p className={styles.doctorBio}>{bio}</p>
+            {/* Card Direito - Informações Adicionais */}
+            <div className={styles.details}>
+              <div className={styles.aboutSection}>
+                <h2 className={styles.txtSob}>Sobre Mim</h2>
+                <p className={styles.doctorBio}>{bio}</p>
+              </div>
+
+              {/* Botão para WhatsApp */}
+              <a
+                href={`https://wa.me/${numero}`}
+                target="_blank"
+                rel="noopener noreferrer"
+                className={styles.whatsappButton}
+              >
+                Fale com o médico via WhatsApp
+              </a>
+            </div>
           </div>
-
-          <div className={styles.txtCard}>
-            <h1 className={styles.doctorName}>{name}</h1>
-            <p className={styles.doctorSpecialty}>{specialty}</p>
-            <p className={styles.doctorCRM}>CRM: {crm}</p>
-            <p className={styles.doctorLocation}>Local: {location.toUpperCase()}</p>
-          </div>
-
-          {/* Card Direito - Informações Adicionais */}
-          <div className={styles.details}>
-            
-
-            
-
-            {/* Botão para WhatsApp */}
-            <a
-              href={`https://wa.me/${numero}`}
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles.whatsappButton}
-            >
-              Fale com o médico via WhatsApp
-            </a>
-          </div>
-        </div>
         </div>
       </div>
       <Footer />
--- src/app/listMed/medicos.jsx
+++ src/app/listMed/medicos.jsx
@@ -1,19 +1,19 @@
 const doctors = [
-  { id: 1, name: 'Dr. João Silva', specialty: 'Urologista', location: 'sp', bio: 'Especialista com 10 anos de experiência.', crm:'1464876', numero:'5514998435929'},
+  { id: 1, name: 'Dr. João Silva', specialty: 'Urologista', location: 'São Paulo', bio: 'Sou urologista com 10 anos de experiência na área, oferecendo cuidados especializados para questões relacionadas ao sistema urinário e reprodutor masculino. Ao longo da minha carreira, me dediquei a fornecer diagnóstico preciso, técnicas inovadoras e cuidados personalizados, sempre com foco no bem-estar do paciente.Com uma formação sólida e experiência em diversas áreas da urologia, como prevenção e tratamento de doenças da próstata, infecções urinárias, litíase renal, disfunções sexuais e saúde masculina, busco sempre estar atualizado com as novidades do campo para garantir o melhor atendimento po', crm:'1464876', numero:'5514998435929'},
 
-  { id: 2, name: 'Dra. Maria Costa', specialty: 'Pediatra', location: 'sp', bio: 'Atende crianças de todas as idades.', crm:'1464876', numero:'5514998435929'},
+  { id: 2, name: 'Dra. Maria Costa', specialty: 'Pediatra', location: 'São Paulo', bio: 'Atende crianças de todas as idades.', crm:'1464876', numero:'5514998435929'},
 
-  { id: 3, name: 'Dr. Carlos Sousa', specialty: 'Cardiologista', location: 'sp', bio: 'Focado em saúde cardiovascular.', crm:'1464876', numero:'5514998435929'},
+  { id: 3, name: 'Dr. Carlos Sousa', specialty: 'Cardiologista', location: 'São Paulo', bio: 'Focado em saúde cardiovascular.', crm:'1464876', numero:'5514998435929'},
 
-  { id: 4, name: 'Dra. Ana Oliveira', specialty: 'Urologista', location: 'rj', bio: 'Atende casos clínicos e cirúrgicos.', crm:'1464876', numero:'5514998435929'},
+  { id: 4, name: 'Dra. Ana Oliveira', specialty: 'Urologista', location: 'São Paulo', bio: 'Atende casos clínicos e cirúrgicos.', crm:'1464876', numero:'5514998435929'},
 
-  { id: 5, name: 'Dr. Pedro Lima', specialty: 'Pediatra', location: 'rj', bio: 'Atendimento humanizado e especializado.', crm:'1464876', numero:'5514998435929'},
+  { id: 5, name: 'Dr. Pedro Lima', specialty: 'Pediatra', location: 'Rio de Janeiro', bio: 'Atendimento humanizado e especializado.', crm:'1464876', numero:'5514998435929'},
   
-  { id: 6, name: 'Dr. Felipe Lima', specialty: 'Cardiologista', location: 'rj', bio: 'Focado em saúde cardiovascular.', crm:'1464876', numero:'5514998435929' },
+  { id: 6, name: 'Dr. Felipe Lima', specialty: 'Cardiologista', location: 'Rio de Janeiro', bio: 'Focado em saúde cardiovascular.', crm:'1464876', numero:'5514998435929' },
 
-  { id: 7, name: 'Dra. Mariani Grassi', specialty: 'Cardiologista', location: 'rj', bio: 'Focado em saúde cardiovascular.', crm:'1464876', numero:'5514998435929' },
+  { id: 7, name: 'Dra. Mariani Grassi', specialty: 'Cardiologista', location: 'Rio de Janeiro', bio: 'Focado em saúde cardiovascular.', crm:'1464876', numero:'5514998435929' },
 
-  { id: 8, name: 'Dr. Ricardão', specialty: 'Cardiologista', location: 'rj', bio: 'Focado em saúde cardiovascular.', crm:'1464876', numero:'5514998435929' },
+  { id: 8, name: 'Dr. Ricardão', specialty: 'Cardiologista', location: 'Rio de Janeiro', bio: 'Focado em saúde cardiovascular.', crm:'1464876', numero:'5514998435929' },
 
   ];
 
